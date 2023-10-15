b57ad9dd00f107f7c2712f7c9840279aac55bf12
tuankietdang52 <dkiet27@gmail.com>
2023-10-16 02:55:43 +0700

    text effect in light story

--- js/code.js
+++ js/code.js
@@ -1,3 +1,42 @@
+$(document).ready(function(){
+    let lighttxt = $('.lightstory');
+    lighttxt.waypoint({
+        handler: function(direction){
+            if (direction == "down"){
+                storytextanimate();
+            }
+        },
+        offset: '70%',
+    })
+})
+
+let countlightstry = false;
+
+function storytextanimate(){
+    if (countlightstry) return;
+    let textwrap = document.querySelector('.lightstory');
+    textwrap.innerHTML = textwrap.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    let sectopacity = $(".lightstory").css("opacity");
+
+    $(".lightstory").css("opacity", 1);
+
+    sectopacity = $(".lightstory").css("opacity");
+
+    let animate = anime.timeline({loop: false}).add({
+        targets: '.lightstory .letter',
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 10*i,
+    })
+
+    countlightstry = true;
+    return animate;
+}
+
 function inpage(pagenav){
    if (pagenav.className == "inpage") return;
    
